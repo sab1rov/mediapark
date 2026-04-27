76b80e6603e57788c4fe6b2967eeb4bf2aa7b52d
Rustambek <rustambekrustamov180@gmail.com>
2026-04-27 11:53:15 +0500

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Route, Routes } from "react-router"
-import { Footer, Header } from "./components"
-import { routes } from "./utils/routes"
+import { Route, Routes } from 'react-router'
+import { Footer, Header } from './components'
+import { routes } from './utils/routes'
 
 function App() {
-
   return (
     <>
       <Header />
       <Routes>
-        {routes.map(item => <Route path={item.path} element={item.element} key={item.path} />)}
+        {routes.map((item) => (
+          <Route path={item.path} element={item.element} key={item.path} />
+        ))}
       </Routes>
       <Footer />
     </>
--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -1,16 +1,16 @@
-import { CartaPage, HomePage, ProductItemPage } from "../pages";
+import { CartaPage, HomePage, ProductItemPage } from '../pages'
 
 export const routes = [
-    {
-        path: "/",
-        element: <HomePage />
-    },
-    {
-        path: "/cart",
-        element: <CartaPage />
-    },
-    {
-        path: "/product-item",
-        element: <ProductItemPage />
-    }
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/cart',
+    element: <CartaPage />,
+  },
+  {
+    path: '/product-item',
+    element: <ProductItemPage />,
+  },
 ]
